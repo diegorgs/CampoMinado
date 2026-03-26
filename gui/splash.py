518f7b2e814d03d820b8d793403daa3884a2f225
Diego <diegorodgs@outlook.com>
2026-03-26 19:17:30 -0300

    Corrigindo Tela Splash

--- gui/splash.py
+++ gui/splash.py
@@ -1,25 +1,18 @@
+import tkinter as tk
+
+
 class SplashScreen:
     def __init__(self, root):
         self.root = root
 
-        self.root.overrideredirect(True)  # remove borda
+        self.root.overrideredirect(True)
         self.root.geometry("300x200")
-
-        # Centralizar janela
-        largura = 300
-        altura = 200
-        x = (self.root.winfo_screenwidth() // 2) - (largura // 2)
-        y = (self.root.winfo_screenheight() // 2) - (altura // 2)
-        self.root.geometry(f"{largura}x{altura}+{x}+{y}")
 
         frame = tk.Frame(root, bg="#F0F0FD")
         frame.pack(fill="both", expand=True)
 
-        label = tk.Label(frame, text="Campo Minado", font=("Arial", 16, "bold"), bg="#F0F0FD")
-        label.pack(pady=40)
+        tk.Label(frame, text="Campo Minado", font=("Arial", 16, "bold"),
+                 bg="#F0F0FD").pack(pady=40)
 
-        sub = tk.Label(frame, text="Carregando...", font=("Arial", 10), bg="#F0F0FD")
-        sub.pack()
-
-    def fechar(self):
-        self.root.destroy()
+        tk.Label(frame, text="Carregando...",
+                 bg="#F0F0FD").pack()
